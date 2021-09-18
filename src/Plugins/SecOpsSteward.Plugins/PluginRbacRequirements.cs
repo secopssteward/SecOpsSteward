86b0b0626bbeac4e3f06029eae0b539ce4d70ac7
Anthony Turner <225599+anthturner@users.noreply.github.com>
2021-09-18 16:59:48 -0400

    remove redundancies

--- src/Plugins/SecOpsSteward.Plugins/PluginRbacRequirements.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SecOpsSteward.Plugins
-{
-    public abstract class PluginRbacRequirements
-    {
-        public abstract string Description { get; protected set; }
-
-        public virtual string TechnicalDescription { get; } = string.Empty;
-    }
-}
